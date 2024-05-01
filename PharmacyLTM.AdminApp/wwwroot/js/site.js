b587885365ce3af4cd4b941b495efacada9340cb
letrungmin <minltgcd210648@fpt.edu.vn>
2024-05-01 10:56:06 +0700

    Add cart items

--- PharmacyLTM.AdminApp/wwwroot/js/site.js
+++ PharmacyLTM.AdminApp/wwwroot/js/site.js
@@ -1,1 +1,23 @@
-﻿// Write your Javascript code.
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$('body').on('click', '.btn-add-cart', function (e) {
+    e.preventDefault();
+    const culture = $('#hidCulture').val();
+    const id = $(this).data('id');
+    $.ajax({
+        type: "POST",
+        url: "/" + culture + '/Cart/AddToCart',
+        data: {
+            id: id,
+            languageId: culture
+        },
+        success: function (res) {
+            console.log(res)
+        },
+        error: function (err) {
+            console.log(err);
+        }
+    });
+})
